975609ac0962c1bbb975d7324efeae790db0fc58
Joe Jiko <me@joejiko.com>
2018-03-17 16:07:16 -0400

    output formatting

--- src/modules/commands.js
+++ src/modules/commands.js
@@ -12,6 +12,7 @@ module.exports = function (bot) {
 
         help: msg => {
             let helpTextEmbed = helpText.get(bot.config.command.symbol, bot.client.user.username, bot.client.user.avatarUrl);
+            console.log(helpTextEmbed);
             msg.channel.send(helpTextEmbed);
         },
 
