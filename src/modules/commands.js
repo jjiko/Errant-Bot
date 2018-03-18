98c4e84d6140c43119a29f947ab89dec784bd7cb
Joe Jiko <me@joejiko.com>
2018-03-18 18:09:45 -0400

    join channel on ready

--- src/modules/commands.js
+++ src/modules/commands.js
@@ -254,7 +254,7 @@ module.exports = function (bot) {
             let volume = msg.details.trim();
             if (volume !== '') {
                 volume = __.math.between(parseInt(volume), 0, 100);
-                volume = (volume / 100) * (2 - 0.5) + 0.5;
+                volume = (volume / 100) * (1.5) + bot.config.stream.volume;
 
                 let track = bot.queue.first;
                 if (track && track.dispatcher)
